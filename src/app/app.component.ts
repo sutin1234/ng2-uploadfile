0ec38eddab07d7ac068da4b6dac661cfad4b4582
sutin <tony.sutin1234@gmail.com>
2017-01-25 10:31:04 +0700

    Add project to github
    Add project to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgUploaderOptions } from 'ngx-uploader';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  uploadFile: any;
+  hasBaseDropZoneOver: boolean = false;
+  options: NgUploaderOptions = {
+    url: 'http://localhost/upload/getUpload.php',
+    filterExtensions: true,
+    allowedExtensions: ['image/png', 'image/jpg']
+  };
+  sizeLimit = 2000000;
+
+  handleUpload(data): void {
+    if (data && data.response) {
+      data = JSON.parse(data.response);
+      this.uploadFile = data;
+      console.log('response from server');
+      console.log(data);
+    }
+  }
+
+  fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  beforeUpload(uploadingFile): void {
+    console.log('init file to upload');
+    console.log(uploadingFile);
+    if (uploadingFile.size > this.sizeLimit) {
+      uploadingFile.setAbort();
+      alert('File is too large');
+    }
+  }
 }
